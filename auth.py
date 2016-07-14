a15efee330e248bc5696555f85af058fe829d6ed
Himanshu Shekhar <himanshushekharb16@gmail.com>
2016-07-14 12:00:40 +0530

    Authentication ported to tinydb

--- auth.py
+++ auth.py
@@ -1,15 +1,8 @@
 try:
-	os
-	shelve
-except NameError:
-	try:
-		import os, shelve
-	except ImportError as e:
-		print (e," Cannot import required modules")
+	from tinydb import TinyDB, where
+except ImportError as e:
+	print (e," Cannot import required modules")
 
-
-ls = os.listdir
-pwd = os.getcwd()
 
 class User:
 	"""Each instance of this class represents a user
@@ -21,22 +14,18 @@ class User:
 	* save_details() : save current details
 	"""
 	def __init__(self, dic):
-		 self.name = dic['name']
-		 self.password = dic['password']
-		 self.homedir = dic['homedir']
-		 self.permission = dic['permission']
-		 self.msg_login = dic['msg_login']
-		 self.msg_quit = dic['msg_quit']
-		 self.filepath = os.path.join(pwd, 'users', self.name)
+		k = list(dic.keys())
+		if 'name' in k and \
+			'password' in k and \
+			'homedir' in k and \
+			'permission' in k and \
+			'msg_login' in k and \
+			'msg_quit' in k:
+			self.record = k
 
 	def save_details(self):
-		dbase = shelve.open(self.filepath)
-		dbase['name'] = self.name
-		dbase['password'] = self.password
-		dbase['homedir'] = self.homedir
-		dbase['permission'] = self.permission
-		dbase['msg_login'] = self.msg_login
-		dbase['msg_quit'] = self.msg_quit
+		dbase = TinyDB('user_database.json')
+		dbase.insert(self.record)
 		dbase.close()
 
 
@@ -50,20 +39,18 @@ class AnonymousUser:
 	* save_details() : save current details
 	"""
 	def __init__(self, dic):
-		self.name = 'anonymous'
-		self.homedir = dic['homedir']
-		self.permission = dic['permission'] or 'elr'
-		self.msg_login = dic['msg_login']
-		self.msg_quit = dic['msg_quit']
-		self.filepath = os.path.join(pwd, 'users', self.name)
+		k = list(dic.keys())
+		if 'password' in k and \
+			'homedir' in k and \
+			'permission' in k and \
+			'msg_login' in k and \
+			'msg_quit' in k:
+			self.record = k
+		self.record['name'] = 'anonymous'
 
 	def save_details(self):
-		dbase = shelve.open(self.filepath)
-		dbase['name'] = 'anonymous'
-		dbase['homedir'] = self.homedir
-		dbase['permission'] = self.permission
-		dbase['msg_login'] = self.msg_login
-		dbase['msg_quit'] = self.msg_quit
+		dbase = TinyDB('user_database.json')
+		dbase.insert(self.record)
 		dbase.close()
 
 
@@ -77,34 +64,39 @@ class Userbase:
 	"""
 
 	def __init__(self):
-		self.userdir = os.path.join(pwd, 'users')
-		self.userlist = ls(self.userdir)
+		self.userlist = self.get_user_list()
 
 	def get_user_list(self):
 		"""Refresh the user list"""
-		self.userlist = ls(self.userdir)
-		for i in range(len(self.userlist)):
-			self.userlist[i] = self.userlist[i][:-3]
+		self.userlist = []
+		dbase = TinyDB('user_database.json')
+		for rec in dbase.all():
+			self.userlist.append(rec['name'])
+		dbase.close()
 		return self.userlist
 
 	def get_user_info(self, username):
 		"""get user object for a user in userlist"""
 		if username in self.get_user_list():
 			# in following either case tmpuser is of respective user type
+			dbase = TinyDB('user_database.json')
+			usr = dbase.search(where('name') == username)
 			if username == 'anonymous':
-				dbase = shelve.open(os.path.join(self.userdir, 'anonymous'), 'r')
-				tmpuser = AnonymousUser({'name':dbase['name'], 'homedir':dbase['homedir'], 'permission':dbase['permission'], 'msg_login':dbase['msg_login'], 'msg_quit':dbase['msg_quit']})
+				# dbase = shelve.open(os.path.join(self.userdir, 'anonymous'), 'r')
+				# tmpuser = AnonymousUser({'name':dbase['name'], 'homedir':dbase['homedir'], 'permission':dbase['permission'], 'msg_login':dbase['msg_login'], 'msg_quit':dbase['msg_quit']})
+				tmpuser = AnonymousUser(usr)
 				dbase.close()
 				return tmpuser
 			else:
-				dbase = shelve.open(os.path.join(self.userdir, username), 'r')
-				tmpuser = User({'name':dbase['name'], 'password':dbase['password'], 'homedir':dbase['homedir'], 'permission':dbase['permission'], 'msg_login':dbase['msg_login'], 'msg_quit':dbase['msg_quit']})
+				# dbase = shelve.open(os.path.join(self.userdir, username), 'r')
+				# tmpuser = User({'name':dbase['name'], 'password':dbase['password'], 'homedir':dbase['homedir'], 'permission':dbase['permission'], 'msg_login':dbase['msg_login'], 'msg_quit':dbase['msg_quit']})
+				tmpuser = AnonymousUser(usr)
 				dbase.close()
 				return tmpuser
 
 	def remove_user(self, username):
 		if username in self.get_user_list():
-			username += '.db'
-			os.remove(os.path.join(self.userdir, username))
+			dbase = TinyDB('user_database.json')
+			dbase.remove(where('name') == username)
 		else:
 			print ("No such user exists!")
