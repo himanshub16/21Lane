b1dc782e0c10040b332d9d3efca97de32d832e58
Himanshu Shekhar <himanshushekharb16@gmail.com>
2016-07-30 12:59:13 +0530

    bug fixes

--- build-tools/windows/install.bat
+++ build-tools/windows/install.bat
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 @echo off
 where python
 cls
@@ -19,30 +18,7 @@ if errorlevel 1 (
 		REM Script from : https://github.com/npocmaka/batch.scripts/blob/master/hybrids/jscript/shortcutJS.bat
 		create_shortcut.bat -linkfile "%userprofile%\Desktop\21Lane.lnk" -target "%userprofile%\21Lane\21lane.bat" -description "21Lane (FTP based file sharing)" -iconlocation "%userprofile%\21Lane\icons\favicon.ico" -workingdirectory "%userprofile%\21Lane\""
 		create_shortcut.bat -linkfile "%appdata%\Microsoft\Windows\Start Menu\21Lane.lnk" -target "%userprofile%\21Lane\21lane.bat" -description "21Lane (FTP based file sharing)" -iconlocation "%userprofile%\21Lane\icons\favicon.ico" -workingdirectory "%userprofile%\21Lane\"
+		echo "Setup complete."
 		pause
 	)
-=======
-@echo off
-where python
-cls
-if errorlevel 1 (
-	echo Python is not installed on your system.
-	echo Make sure Python 3.5.x is installed and PATH is configured.
-	pause
-) else (
-	where pip
-	cls 
-	if errorlevel 1 (
-		echo Missing pip.
-		echo Make sure your Python installation is complete.
-	) else (
-		start /wait python setup.py install
-		echo Installing required modules
-		pip install -r requirements.txt
-		REM Don't create desktop shortcut .. execution issues
-		REM cscript create_shortcut.vbs
-		REM del create_shortcut.vbs
-		pause
-	)
->>>>>>> 71327457a48f59f893714a5c32db9df84e8110f5
 )
