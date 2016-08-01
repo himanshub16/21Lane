056b97837f4ebab5e0d4d79918126ee15a5a5e51
Himanshu Shekhar <himanshushekharb16@gmail.com>
2016-08-01 17:13:39 +0530

    separated 21Exchange to another repo

--- 21Exchange/serve.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-if [%1] == [] (
-	set port=8000
-) else (
-	set port=%1
-)
-
-start /wait python http.server --cgi %port%
-exit
--- 21Exchange/serve.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if [ "$1" = "" ]; then
-	port=8000
-else
-	port="$1"
-fi
-chmod +x cgi-bin/ -R
-python3 -m http.server --cgi $port
