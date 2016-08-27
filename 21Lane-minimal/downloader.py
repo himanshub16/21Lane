a1757aeeadcd2fa4c99e8bbf39ae65125b442d62
Himanshu Shekhar <himanshushekharb16@gmail.com>
2016-08-27 09:06:43 +0530

    Download progress bar added

--- 21Lane-minimal/downloader.py
+++ 21Lane-minimal/downloader.py
@@ -56,68 +56,95 @@ class Downloader:
 
 		# Thread to monitor download stats
 		# ... commented as it caused lags.
+		# changed completion label with progress bar
 		# will try to implement in future versions
-		# def stats_monitor(filename, total_size, guiwidget, destpath):
-		# 	if destpath.startswith('/'):
-		# 		destpath = destpath[1:]
-		# 	downloadPath = self.downloadPath
-		# 	if (destpath != ''):
-		# 		downloadPath = os.path.join(downloadPath, destpath)
-		# 	destination = os.path.join(downloadPath, destpath)
-		# 	guiwidget['state'] = 'running'
-		# 	while guiwidget['state'] =='running':
-		# 		try:
-		# 			size = os.path.getsize(os.path.join(destination, filename))
-		# 			guiwidget['label'].setText( str(100 * int(size//total_size))+' % Running' )
-		# 			time.sleep(2)
-		# 		except Exception as e:
-		# 			pass
+		def stats_monitor(filename, total_size, guiwidget, destpath):
+			try:
+				if destpath.startswith('/'):
+					destpath = destpath[1:]
+				downloadPath = self.downloadPath
+				if (destpath != ''):
+					downloadPath = os.path.join(downloadPath, destpath)
+				destination = os.path.join(downloadPath, destpath)
+				guiwidget['state'] = 'running'
+				print("gui widget state running")
+				print(destination, filename)
+				while guiwidget['state'] =='running':
+					try:
+						size = os.path.getsize(os.path.join(destination, filename))
+						completionPercent = ((100 * size) / float(total_size)) 
+						print(completionPercent)
+						guiwidget['pbar'].setValue(completionPercent)
+						time.sleep(1)
+					except FileNotFoundError as e:
+						print("File not found")
+						pass
+					except Exception as e:
+						raise e
+			except RuntimeError as e:
+				return
+			
+			except Exception as e:
+				raise e
 
 		while True and self.execute:
 			self.lock.acquire()
 			node = self.downloadQueue.get()
 			self.lock.release()
-			completion = False
-			total_size = node.filesize
-			guiwidget = node.guiwidget
-			guiwidget['statusicon'].setText("In Queue")
-			client = FTPClient(node.hostname, node.port)
-			filename = node.filename
-			destpath = node.destpath
-			# th = threading.Thread(target=stats_monitor, args=(filename, total_size, guiwidget, destpath,))
-			# th.setDaemon(False)
-			# th.start()
-			node.tries += 1
-			node.guiwidget['label'].setText("Downloading")
-			node.guiwidget['statusicon'].setPixmap(QPixmap('icons/running.svg'))
+			try:
+				completion = False
+				total_size = node.filesize
+				guiwidget = node.guiwidget
+				guiwidget['statusicon'].setToolTip("In Queue")
+
+				client = FTPClient(node.hostname, node.port)
+				filename = node.filename
+				destpath = node.destpath
+				th = threading.Thread(target=stats_monitor, args=(filename, total_size, guiwidget, destpath,))
+				th.setDaemon(False)
+				th.start()
+				node.tries += 1
+				# node.guiwidget['label'].setText("Downloading")
+				node.guiwidget['statusicon'].setPixmap(QPixmap('icons/running.svg'))
+				node.guiwidget['statusicon'].setToolTip('Downloading')
+			except RuntimeError as e:
+				continue
+
 			try:
 				if (node.tries < 4 and client.downloadFile(pathname=node.pathname, filename=node.filename, destpath=node.destpath)):
-					guiwidget['label'].setText("Completed")
+					# guiwidget['label'].setText("Completed")
 					guiwidget['statusicon'].setPixmap(QPixmap('icons/complete.svg'))
+					guiwidget['statusicon'].setToolTip('Completed')
 					node.guiwidget['state'] = 'completed'
 					
 				else:
 					if (node.tries == 4):
-						guiwidget['label'].setText("Failed")
+						# guiwidget['label'].setText("Failed")
 						guiwidget['statusicon'].setPixmap(QPixmap('icons/failed.svg'))
+						guiwidget['statusicon'].setToolTip('Failed')
 						node.guiwidget['state'] = 'failed'
 					else:
 						self.downloadQueue.put(node)
-						guiwidget['label'].setText("Waiting")
+						# guiwidget['label'].setText("Waiting")
 						guiwidget['statusicon'].setPixmap(QPixmap('icons/wait.svg'))
+						guiwidget['statusicon'].setToolTip('Waiting')
 						node.guiwidget['state'] = 'waiting'
 				self.downloadQueue.task_done()
 				completion = True
 
+			except RuntimeError as e:
+				pass
+
 			except Exception as e:
 				node.guiwidget['statusicon'].setPixmap(QPixmap('icons/failed.svg'))
-				node.guiwidget['label'].setText("Network error. Retry")
+				node.guiwidget['statusicon'].setToolTip("Network Error")
+				# node.guiwidget['label'].setText("Network error. Retry")
 				raise e
 				pass
 
 	def addEntry(self, pathname, filesize, filename, guiwidget, destpath='', hostname='localhost', port=2121):
 		node = Node(hostname=hostname, port=port, pathname=pathname, filesize=filesize, filename=filename, guiwidget=guiwidget, destpath=destpath)
 		if not Node:
-			node.guiWidget['state'] = 'failed'
+			node.guiwidget['state'] = 'failed'
 			return False
 		self.downloadQueue.put(node)
--- 21Lane-minimal/ftp_browser.py
+++ 21Lane-minimal/ftp_browser.py
@@ -1,12 +1,20 @@
-import sys, os
+import sys, os, json
+import requests
+from datetime import datetime
 from ftpclient import FTPClient
 from downloader import Downloader
 
 from PyQt5.QtWidgets import (QWidget, QApplication, QPushButton, QLabel, QAction, \
-		QGroupBox, QHBoxLayout, QVBoxLayout, QFormLayout, QLineEdit, QFileDialog, \
-		QScrollArea, QMessageBox, QGridLayout, QSpacerItem, QSizePolicy )
+		QGroupBox, QHBoxLayout, QVBoxLayout, QFormLayout, QLineEdit, QFileDialog, QProgressBar,\
+		QScrollArea, QMessageBox, QGridLayout, QSpacerItem, QSizePolicy, QRadioButton, QButtonGroup )
 from PyQt5.QtGui import QIcon, QCloseEvent, QPixmap
 from PyQt5.QtCore import Qt
+
+def mylog(ar):
+	f = open('log.txt', 'a')
+	f.write(str(datetime.now()) + " " + ar + "\n")
+	f.close()
+
 
 def convertSize(size):
 	# size is in bytes
@@ -33,7 +41,7 @@ def convertSize(size):
 				
 
 class FTPBrowser(QWidget):
-	def __init__(self, hostname, port, server_name):
+	def __init__(self, hostname, port, server_name, exchange_url, target_id):
 		super().__init__()
 		try:
 			self.hostname = hostname
@@ -42,6 +50,8 @@ class FTPBrowser(QWidget):
 			self.pwd = '/'
 			self.oldpwd = '/'
 			self.client = FTPClient(hostname=self.hostname, port=self.port)
+			self.exchange_url = exchange_url
+			self.target_id = target_id
 		except Exception as e:
 			print("Some error occured")
 		self.initUI()
@@ -50,7 +60,7 @@ class FTPBrowser(QWidget):
 	
 	def initUI(self):
 
-		self.addressBar = QGroupBox()
+		self.addressBar = QGroupBox("Navigation")
 		self.aBLayout = QHBoxLayout()
 		self.addressBox = QLineEdit()
 		self.addressBox.setPlaceholderText("Enter path to continue")
@@ -66,6 +76,42 @@ class FTPBrowser(QWidget):
 		self.aBLayout.addWidget(self.addressBox)
 		self.aBLayout.addWidget(self.goBtn)
 		self.addressBar.setLayout(self.aBLayout)
+
+
+		self.searchContainer = QGroupBox("Search Box")
+		self.searchContainerLayout = QVBoxLayout()
+		self.searchContainer.setLayout(self.searchContainerLayout)
+
+		self.actionsGroupBox = QGroupBox()
+		self.aGBLayout = QHBoxLayout()
+		self.actionsGroupBox.setLayout(self.aGBLayout)
+		self.searchbox = QLineEdit()
+		self.searchbox.setPlaceholderText("Search for file")
+		self.searchBtn = QPushButton(QIcon('icons/search.png'), '')
+		self.searchBtn.clicked[bool].connect(self.searchQuery)
+		self.searchbox.returnPressed.connect(self.searchBtn.click)
+		self.aGBLayout.addWidget(self.searchbox)
+		self.aGBLayout.addWidget(self.searchBtn)
+
+		self.searchCategoryBox = QGroupBox("Select file type to search")
+		self.sCLayout = QHBoxLayout()
+		self.searchCategoryBox.setLayout(self.sCLayout)
+		self.radioButtonGroup = QButtonGroup()
+		self.categoryList = ['Application', 'Video', 'Audio', 'Text', 'Other']
+		self.categoryRadioList = []
+		for each in self.categoryList:
+			self.categoryRadioList.append(QRadioButton(each))
+
+		counter = 1
+		# 1 : Application, 2 : Video, 3 : Audio, 4 : Docuement, 5 : Other
+		for each in self.categoryRadioList:
+			self.sCLayout.addWidget(each)
+			self.radioButtonGroup.addButton(each)
+			self.radioButtonGroup.setId(each, counter)
+			counter += 1
+
+		self.searchContainerLayout.addWidget(self.actionsGroupBox)
+		self.searchContainerLayout.addWidget(self.searchCategoryBox)
 
 
 		self.clientBox = QGroupBox("File listing")
@@ -88,6 +134,7 @@ class FTPBrowser(QWidget):
 
 		mainLayout = QVBoxLayout(self)
 		mainLayout.addWidget(self.addressBar)
+		mainLayout.addWidget(self.searchContainer)
 		mainLayout.addWidget(self.vscroll)
 		mainLayout.addWidget(self.hscroll)
 		self.setWindowTitle(self.windowTitle)
@@ -105,6 +152,103 @@ class FTPBrowser(QWidget):
 			event.accept()
 		else:
 			event.ignore()
+
+	def searchQuery(self):
+		try:
+			if len(self.searchbox.text()) < 3:
+				QMessageBox.information(self, "Oops", "That's an absurd search!", QMessageBox.Ok, QMessageBox.Ok)
+				return  
+
+			searchCategory = self.radioButtonGroup.checkedId()
+			if searchCategory < 1: 
+				QMessageBox.information(self, "Oops", "Please select a category to search from.", QMessageBox.Ok, QMessageBox.Ok)
+				return
+
+			if 'session_id' in os.listdir(os.getcwd()):
+				sessidFile = open('session_id', 'r')
+				self.session_id = sessidFile.read().strip()
+				sessidFile.close()
+			else:
+				self.session_id = None
+
+			uri = self.exchange_url + '/cgi-bin/search.py'
+			headers = {'user-agent':'21Lane'}
+
+			print(searchCategory, self.session_id)
+			r = requests.post(uri, data={'q':'arrow', 'category':searchCategory, 'target':self.target_id }, cookies={'session_id':self.session_id}, headers=headers, proxies=None, timeout=5)
+			responseJSON = ''
+			print (r.text)
+			# return
+			if r.status_code == 200:
+				if r.text.strip() == 'unauthorized':
+					QMessageBox.warning(self, 'Unauthorized', "You are not connected to this exchange.", QMessageBox.Ok, QMessageBox.Ok)
+					return
+
+				try:
+					responseJSON = json.loads(r.text)
+				except Exception as e:
+					QMessageBox.critical(self, 'Decode error', "Couldn't understand response. Please report at the exchange.", QMessageBox.Ok, QMessageBox.Ok)
+					raise e
+					return
+
+				if len(responseJSON) == 0:
+					QMessageBox.information(self, "Aww", "No results found", QMessageBox.Ok, QMessageBox.Ok)
+					return
+
+				while(self.cBLayout.count()):
+					try:
+						self.cBLayout.takeAt(0).widget().deleteLater()
+					except Exception as e:
+						pass
+				
+				self.cBLayout.sizeHint()
+
+				headerAction = QLabel('')
+				headerSize = QLabel('Shared Size')
+				headerName = QLabel('Name')
+				headerMime = QLabel('Mime Type')
+				headerName.setStyleSheet("font-weight: bold")
+				headerSize.setStyleSheet("font-weight: bold")
+				headerMime.setStyleSheet("font-weight: bold")
+				self.cBLayout.addWidget(headerAction, 0, 0)
+				self.cBLayout.addWidget(headerSize, 0, 1)
+				self.cBLayout.addWidget(headerMime, 0, 2)
+				self.cBLayout.addWidget(headerName, 0, 3)
+				counter = 1
+
+				for sessid in list( responseJSON.keys() ):
+					host, server_name = sessid.split('#')
+					ip, port = host.split(':')
+					
+					for entry in responseJSON[sessid]:
+						downloadAction = QPushButton(QIcon('icons/download.png'), '')
+						downloadAction.clicked.connect(self.downloadFile(filename=entry['filename'], pathname=entry['fullpath'], filesize=entry['size']))
+						sizeLabel = QLabel(convertSize((entry['size'])))
+						mimeLabel = QLabel(entry['mimetype'])
+						nameLabel = QLabel(entry['filename'])
+						downloadAction.setMaximumWidth(25)
+						sizeLabel.setMaximumWidth(100)
+						self.cBLayout.addWidget(downloadAction, counter, 0)
+						self.cBLayout.addWidget(sizeLabel, counter, 1)
+						self.cBLayout.addWidget(mimeLabel, counter, 2)
+						self.cBLayout.addWidget(nameLabel, counter, 3)
+						
+						counter += 1
+
+				self.cBLayout.addItem(self.cBSpacer, counter, 5, Qt.AlignTop)
+				self.cBLayout.setColumnStretch(3, 1)
+				self.cBLayout.setHorizontalSpacing(10)
+
+
+		except (requests.exceptions.ConnectionError, requests.exceptions.HTTPError, ConnectionAbortedError, requests.exceptions.Timeout) as e:
+			QMessageBox.critical(self, 'Error', 'Network error!', QMessageBox.Ok, QMessageBox.Ok)
+			raise e
+		except Exception as e:
+			# first close any open file to avoid permissions error in windows, and other similar errors
+			QMessageBox.critical(self, 'Error', "Some error occured!", QMessageBox.Ok, QMessageBox.Ok)
+			mylog(str(e) + ' ' + 'is the error')
+			raise e
+
 
 	def changeBaseURI(self):
 		sender = self.sender()
@@ -141,17 +285,23 @@ class FTPBrowser(QWidget):
 
 	def downloadFile(self, filename, pathname, filesize):
 		def makeDownloadEntry():
-			downloadItem = QGroupBox(filename)
+			downloadItem = QGroupBox(convertSize(filesize) + ' ' + filename)
+			downloadItem.setToolTip(filename)
 			downloadItem.setFixedWidth(200)
 			dilayout = QHBoxLayout()
 			downloadItem.setLayout(dilayout)
 			statusIcon = QLabel()
 			statusIcon.setPixmap(QPixmap('icons/wait.svg'))
+			statusIcon.setToolTip("In Queue")
 			statusIcon.setFixedSize(25, 25)
-			completionLabel = QLabel('In Queue')
+			# completionLabel = QLabel('In Queue')
+			pBar = QProgressBar()
+			pBar.setValue(0)
 			dilayout.addWidget(statusIcon)
-			dilayout.addWidget(completionLabel)
+			# dilayout.addWidget(completionLabel)
+			dilayout.addWidget(pBar)
 			btn = QPushButton(QIcon('icons/cancel.png'), '')
+			btn.setToolTip("Delete this box \n(cancel download if not running)")
 			btn.setMaximumSize(25,25)
 			btn.clicked.connect(downloadItem.deleteLater)
 			browserbtn = QPushButton(QIcon('icons/browse.png'), '')
@@ -161,7 +311,7 @@ class FTPBrowser(QWidget):
 			dilayout.addWidget(browserbtn)
 			dilayout.addWidget(btn)
 			self.dbLayout.addWidget(downloadItem)
-			self.downman.addEntry(hostname=self.hostname, port=self.port, pathname=pathname, filesize=filesize, filename=filename, guiwidget={"state":'in queue', "statusicon":statusIcon, 'groupbox':downloadItem, 'btn':btn, 'label':completionLabel })
+			self.downman.addEntry(hostname=self.hostname, port=self.port, pathname=pathname, filesize=filesize, filename=filename, guiwidget={"state":'in queue', "statusicon":statusIcon, 'groupbox':downloadItem, 'btn':btn, 'pbar':pBar })
 
 		return makeDownloadEntry
 	
@@ -248,11 +398,17 @@ class FTPBrowser(QWidget):
 			
 
 if __name__=="__main__":
-	if (len(sys.argv) < 4):
+	try:
+		os.environ.pop('all_proxy')
+	except Exception as e:
+		pass
+		
+	if (len(sys.argv) < 5):
 		print('insufficient arguments')
 		sys.exit(1)
+
 	app = QApplication([])
 	app.setWindowIcon(QIcon('icons/favicon.ico'))
-	ui = FTPBrowser(hostname=sys.argv[1], port=sys.argv[2], server_name=sys.argv[3])
+	ui = FTPBrowser(hostname=sys.argv[1], port=sys.argv[2], server_name=sys.argv[3], exchange_url=sys.argv[4], target_id = sys.argv[5].strip())
 	sys.exit(app.exec_())
 
