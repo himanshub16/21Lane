9035a6ef86508a10d1b44160925060ec9dae0bc0
Himanshu Shekhar <himanshushekharb16@gmail.com>
2016-12-16 19:20:43 +0530

    downloader test created

--- 21Lane/downloader-test.py
+++ 21Lane/downloader-test.py
@@ -1,5 +1,5 @@
-from downloader import DownloadItem, Downloader
-from threading import BoundedSemaphore, Thread
+from downloader import DownloadItem
+from downman import DownloadManager
 
 filelist = [ {
         "filename": "file1",
@@ -39,33 +39,12 @@ for item in filelist:
     di = DownloadItem(item["filename"], item["host"], item["port"], item["source"], item["destination"], 780, None)
     dilist.append(di)
 
+m = DownloadManager()
+for di in dilist:
+    m.addItem(di)
+    # print (m.downloadQueue.__len__())
+    # print(m.batchProcessor)
 
-workerlist = []
-sem = BoundedSemaphore(3)
-for i in range(3):
-	workerlist.append(Downloader(sem))
 
-
-i = 2
-print(workerlist)
-print()
-pool = []
-while dilist:
-	di = dilist.pop()
-	avail = [ worker for worker in workerlist if not worker.running ]
-	print(len(avail), 'workers available')
-	while len(avail) == 0:
-		avail = [ worker for worker in workerlist if not worker.running ]
-
-	worker = avail[0]
-	print (di.filename, 'with',workerlist.index(worker))
-	worker.update(di)
-	sem.acquire()
-	th = Thread(target=worker.download)
-	th.start()
-	while (not worker.running):
-		pass
-	pool.append(th)
-
-for thread in pool:
-	thread.join()
+a=input()
+m.stopDownloader()
