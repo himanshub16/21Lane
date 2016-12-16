13b181c53cc6521340495b55d813221041326006
Himanshu Shekhar <himanshushekharb16@gmail.com>
2016-12-16 20:59:04 +0530

    server test created

---
+++ 21Lane/server-test.py
@@ -0,0 +1,24 @@
+from server import *
+from time import sleep
+
+s = Server()
+p = int(input("Enter port : "))
+print ("port %d is available " % (p))
+s.setPort(p)
+s.setSharedDirectory("/home/himanshu")
+s.setBandwidth(20)
+s.startServer()
+
+i = 0
+while i < 5:
+    sleep(0.5)
+    print ("t=", i*0.5, "server is alive", s.server_proc.is_alive())
+    i += 1
+
+print ("port", p, "is available", isPortAvailable(p))
+print ("stopping server")
+s.stopServer()
+print ("server is alive", s.server_proc.is_alive())
+print ("port", p, "is available", isPortAvailable(p))
+
+
