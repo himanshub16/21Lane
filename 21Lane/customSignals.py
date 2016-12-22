bb92863f46c5b85bfa38babbdfe7cac66ec2f554
Himanshu Shekhar <himanshushekharb16@gmail.com>
2016-12-23 04:03:02 +0530

    custom signals for updating stats

--- 21Lane/customSignals.py
+++ 21Lane/customSignals.py
@@ -3,8 +3,38 @@ from PyQt5.QtCore import pyqtSignal, QObject
 class DownloadItemUpdater(QObject):
     signal = pyqtSignal()
 
-    def updateValues():
+    def updateValues(self):
         self.signal.emit()
     
-    def raiseError():
+    def raiseErrors(self):
         self.signal.emit()
+
+
+class ServerStatsUpdater(QObject):
+    clientConnect = pyqtSignal()
+    clientDisconnect = pyqtSignal()
+    fileTransfer = pyqtSignal(int)
+
+    def connected(self):
+        self.clientConnect.emit()
+
+    def disconnected(self):
+        self.clientDisconnect.emit()
+
+    def transferred(self, filesize):
+        self.fileTransfer.emit(filesize)
+
+
+class DownloadItemUpdater(QObject):
+    progress = pyqtSignal(int)
+    complete = pyqtSignal()
+    error = pyqtSignal()
+
+    def updateProgress(self, value):
+        self.progress.emit(value)
+    
+    def completed(self):
+        self.complete.emit()
+
+    def raiseError(self):
+        self.error.emit()
